8fe0ee2987d2ae0cc2206a75e84933f69297e9af
Mateus Nava <nava.mateus@gmail.com>
2017-05-08 23:01:27 -0300

    padronizações

--- wp_revista.php
+++ wp_revista.php
@@ -16,16 +16,17 @@ Tags:
 */
 
 
-defined( 'ABSPATH' ) or die( 'Nop!' );
-
+/**
+ * Instantiate IssueM class, require helper files
+ *
+ * @since 1.2.0
+ */
 function inicializar()
 {
-	require_once( 'adiciona_custom_types.php' );
-	require_once( 'adiciona_taxonomias.php' );
-	require_once( 'adiciona_meta_term_status_edicao.php' );
-	require_once( 'adiciona_meta_term_data_edicao.php' );
-  	require_once( 'add-custom-admin-artigos.php' );
-
+  require_once( 'adiciona_custom_types.php' );
+  require_once( 'adiciona_taxonomias.php' );
+  require_once( 'adiciona_avaliacoes' );
+  require_once( 'adiciona_admin_avaliadores');
 }
 add_action( 'plugins_loaded', 'inicializar', 9999999999 ); //espera todos os plugins serem carregados
 
