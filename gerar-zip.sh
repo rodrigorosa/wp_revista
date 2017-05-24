f4b5265a3fab9e9354f236fb54586dc5a4052eb7
Mateus Nava <nava.mateus@gmail.com>
2017-05-23 22:19:50 -0300

    Alterado nome do zip

--- gerar-zip.sh
+++ gerar-zip.sh
@@ -2,7 +2,7 @@
 
 echo $'\n\n\e[1;34m'"$*"$'Gerando pacote...\n\n\e[0m'
 
-rm -rf dist/plugin.zip
-zip dist/plugin.zip ./*.php
+rm -rf dist/wp-revista.zip
+zip dist/wp-revista.zip ./*.php
 
 echo $'\n\n\n\e[1;34m'"$*"$'Pacote gerado com sucesso!\e[0m'
