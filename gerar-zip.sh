489a336c33f6302c336a7c56e57acd6c81d24545
Rodrigo Rosa <rodrigocrosa@gmail.com>
2017-05-26 19:53:07 -0300

    Adicionado css ao script de empacotamento

--- gerar-zip.sh
+++ gerar-zip.sh
@@ -3,6 +3,6 @@
 echo $'\n\n\e[1;34m'"$*"$'Gerando pacote...\n\n\e[0m'
 
 rm -rf dist/wp-revista.zip
-zip dist/wp-revista.zip ./*.php
+zip dist/wp-revista.zip ./*.php ./*.css
 
 echo $'\n\n\n\e[1;34m'"$*"$'Pacote gerado com sucesso!\e[0m'
